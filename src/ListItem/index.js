0cca141f2e6ba467e79acfc3523f748a4f8e84a7
Karolina.Vaittinen <karolinavaittinen@gmail.com>
2023-05-29 10:44:11 +0200

    pridano expand menu

--- src/ListItem/index.js
+++ src/ListItem/index.js
@@ -1,30 +1,40 @@
-import './style.css';
+import "./style.css";
 
 export const ListItem = (props) => {
   const { day, item } = props;
-  const { id, done, product, amount, unit } = item;
+  const { id, done, product, amount, unit, expanded } = item;
 
-  let tickClass = '';
+  let tickClass = "";
   if (done) {
-    tickClass = ' btn-tick--on';
+    tickClass = " btn-tick--on";
   }
 
-  const element = document.createElement('div');
-  element.classList.add('list-item');
+  const element = document.createElement("div");
+  element.classList.add("list-item");
+  if (expanded) {
+    element.classList.add("list-item--expanded");
+  }
   element.innerHTML = `
+  <div class="list-item__toolbar">Tlačítka</div>
     <button class="icon-btn btn-tick${tickClass}"></button>
     <div class="list-item__body">
       <div class="list-item__product">${product}</div>
       <div class="list-item__amount">${amount} ${unit}</div>
     </div>
+    <div class="list-item__detail">
+      Detail položky
+    </div>
+    <div class="list-item__menu">
+      <button class="icon-btn btn-menu"></button>
+    </div>
   `;
 
   const handleTick = () => {
     fetch(`https://nakupy.kodim.app/api/me/week/${day}/items/${id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
       body: JSON.stringify({
         done: !done,
@@ -32,14 +42,22 @@ export const ListItem = (props) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        element.replaceWith(
-          ListItem(
-            { day, item: data.result }
-          )
-        );
+        element.replaceWith(ListItem({ day, item: data.result }));
       });
   };
 
-  element.querySelector('.btn-tick').addEventListener('click', handleTick);
+  const expandMenu = () => {
+    element.replaceWith(
+      ListItem({
+        day: day,
+        item: item,
+        expanded: !expanded,
+      })
+    );
+  };
+
+  element.querySelector(".btn-menu").addEventListener("click", expandMenu);
+
+  element.querySelector(".btn-tick").addEventListener("click", handleTick);
   return element;
 };
